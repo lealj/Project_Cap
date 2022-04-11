b6a8005304609e7cf69740233f07066fa6b51a74
jakew54 <jakew54@gmail.com>
2022-04-10 21:48:21 -0400

    scraping
    1) Added scrape() to main to put data into dfs
    2) Added updated excel sheets with proper format

--- main.py
+++ main.py
@@ -3,6 +3,16 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+
+
+def scrape():
+    filenameGDP = r'GDP.xls'
+    filenameDR = r'DeathRate.xls'
+    dfGDP = pd.read_excel(filenameGDP)
+    dfDR = pd.read_excel(filenameDR)
+    print(dfGDP)
+    print(dfDR)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +22,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    scrape()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
