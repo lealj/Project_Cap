6806c91fc619d0ac6c87a46a57bc500026abe94a
jleal24 <73616612+jleal24@users.noreply.github.com>
2022-04-17 18:40:30 -0400

    added functions

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn import tree
+from sklearn import preprocessing
 import seaborn as sns
 
 regions = ['Latin America & Caribbean', 'South Asia', 'Sub-Saharan Africa', 'Europe & Central Asia', 'Middle East & North Africa', 'East Asia & Pacific', 'North America']
 filenameGDP = r'GDP_Per_Capita.xls'
 filenameDR = r'DeathRate.xls'
+filenameGDP1 = r'GDP.xls'
 
 
 def scrape(): #scrapes excel sheets and places into dataframes
@@ -19,7 +25,11 @@ def scrape(): #scrapes excel sheets and places into dataframes
 
     dfDR = pd.read_excel(filenameDR)
     dfDR = dfDR.fillna(-1)
-    return dfGDP, dfDR
+    # return dfGDP, dfDR
+
+    dfGDP1 = pd.read_excel(filenameGDP1)# for GDP not per Capita
+    dfDR1 = pd.read_excel(filenameDR)# for DR with NaN
+    return dfGDP, dfDR, dfGDP1, dfDR1
 
 
 def getDates(dfGDP): #return list of dates for graphing purposes (1960-2020)
@@ -33,24 +43,24 @@ def getDates(dfGDP): #return list of dates for graphing purposes (1960-2020)
 def getData(index, df): #return data of country at index in dataframe df
     data = []
     for (columnName, columnData) in df.iteritems():
-        if (columnName.isnumeric()):
+        if columnName.isnumeric():
             data.append(columnData[index])
     return data
 
 
 def getName(index, df): #return name of country at index in dataframe df
     for (columnName, columnData) in df.iteritems():
-        if (columnName == 'Country Name'):
+        if columnName == 'Country Name':
             name = columnData[index]
     return name
 
 
 #seven regions
 def getRegionGroups(region): #returns indexes of all tuples of given region - used with mappedlist (regionMap)
-    df, dfDR = scrape()
+    df, dfDR, g, d = scrape()
     indexes = []
     for (columnName, columnData) in df.iteritems():
-        if (columnName == 'Region'):
+        if columnName == 'Region':
             for i in range(0, len(columnData)):
                 if region == columnData[i]:
                     indexes.append(i)
@@ -116,7 +126,58 @@ def plot_finalize(): #finalize plot and show plot
     plt.xlabel('Time')
     # plt.show()
 
+# ####################################
+def cleanData(dfGDP, dfDR):#get rid of the features besides IncomeGroup and GDP and Death rate
+    GDP_ = dfGDP.drop(labels=['Country Code', 'Region', 'Indicator Name','Indicator Code', "Country Name"], axis=1)
+    GDP_ = GDP_.dropna(subset=["IncomeGroup"])
+    GDP_ = GDP_.replace(-1, 0)
 
+    DR_ = dfDR.drop(labels=['Country Code', 'Region', 'Indicator Name','Indicator Code', "Country Name"], axis=1)
+    DR_ = DR_.dropna(subset=["IncomeGroup"])
+    DR_ = DR_.fillna(0)
+
+    return GDP_, DR_
+
+def getXy(GDP_, DR_):
+    #normalizing the data and generated the X and y
+    y_ = GDP_.loc[:, 'IncomeGroup'].to_numpy()
+    X1 = GDP_.iloc[:, 1:62]
+    X1d = preprocessing.normalize(X1, axis=0)
+    X2 = DR_.iloc[:, 1:62]
+    X2d = preprocessing.normalize(X2, axis=0)
+    X_ = np.concatenate((X1d, X2d), axis=1)
+    return X_, y_
+
+def getAvg(GDP_, DR_):
+    avgGDP = GDP_.mean(axis=1)
+    avgDR = DR_.mean(axis=1)
+    Xavg_ = np.vstack((avgGDP, avgDR))
+    return Xavg_
+
+def MLPandcm(X_train_, X_test_, y_train_, y_test_):
+    clf = MLPClassifier(activation='tanh', hidden_layer_sizes=(200,150), max_iter=1500)
+    clf.fit(X_train_, y_train_)
+    test_pred = clf.predict(X_test_)
+    train_pred = clf.predict(X_train_)
+    # print("MLP Classifier")
+    # print("The testing accuracy score is: " + str(accuracy_score(y_test_, test_pred)))
+    # print("The testing accuracy score for X_train is: " + str(accuracy_score(y_train_, train_pred_)))
+    plot_confusion_matrix(clf, X_test_, y_test_)
+    plt.xticks(rotation=45)
+    # plt.show()
+
+def DTandcm(X_train_, X_test_, y_train_, y_test_):
+    clf = tree.DecisionTreeClassifier().fit(X_train_, y_train_)
+    test_pred = clf.predict(X_test_)
+    train_pred = clf.predict(X_train_)
+    # print("Decision Tree Classifier")
+    # print("The testing accuracy score is: " + str(accuracy_score(y_test_, clf.predict(X_test_))))
+    # print("The testing accuracy score for X_train is: " + str(accuracy_score(y_train_, clf.predict(X_train_))))
+    plot_confusion_matrix(clf, X_test_, y_test_)
+    plt.xticks(rotation=45)
+    # plt.show()
+
+# ###########################################
 def get_country_dictionary(gdp):
     countries = gdp.loc[:, 'Country Name']
     countriesDict = countries.to_dict()
@@ -124,17 +185,17 @@ def get_country_dictionary(gdp):
 
 
 def get_total(gdp, dr):
-    GDP = gdp.loc[0, ~gdp.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
-    DR = dr.loc[0, ~dr.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
+    GDP_ = gdp.loc[0, ~gdp.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
+    DR_ = dr.loc[0, ~dr.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
 
-    total_gdp_vals, total_dr_vals, total_country_index_vals = get_total_helper(GDP, DR, 0)
+    total_gdp_vals, total_dr_vals, total_country_index_vals = get_total_helper(GDP_, DR_, 0)
     # start i at 1
     for i in range(1, len(gdp)):
         index = i
         # trim columns to only get years/values
-        GDP = gdp.loc[i, ~gdp.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
-        DR = dr.loc[i, ~dr.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
-        country_gdp, country_dr, country_index = get_total_helper(GDP, DR, index)
+        GDP_ = gdp.loc[i, ~gdp.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
+        DR_ = dr.loc[i, ~dr.columns.isin(['Country Name', 'Country Code', 'Indicator Code', 'Region', 'IncomeGroup', 'Indicator Name'])]
+        country_gdp, country_dr, country_index = get_total_helper(GDP_, DR_, index)
 
         # add return arrays to the end of our total storage arrays
         total_gdp_vals = np.concatenate((total_gdp_vals, country_gdp))
@@ -159,8 +220,8 @@ def get_total_helper(gdp, dr, index):
     return dataGDP, dataDR, country_index
 
 
-def plot_regression(x_test, y_test, y_pred, title):
-    plt.scatter(x_test, y_test)
+def plot_regression(x_test, y_test_, y_pred, title):
+    plt.scatter(x_test, y_test_)
     plt.plot(x_test, y_pred, color="black", linewidth=3)
     # add amount to y label(mil, bil, tril)
     plt.xlabel('Death Rate crude per 1000 people')
@@ -185,18 +246,18 @@ def regression(gdp, dr, index):
     mod_gdp = np.log10(mod_gdp)
 
     # regression on all data points
-    x_train, x_test, y_train, y_test = train_test_split(mod_dr, mod_gdp, test_size=0.2)
+    x_train, x_test, y_Train, y_Test = train_test_split(mod_dr, mod_gdp, test_size=0.2)
     regr = linear_model.LinearRegression()
-    regr.fit(x_train, y_train)
+    regr.fit(x_train, y_Train)
     y_pred = regr.predict(x_test)
 
     # print(regr.coef_)
-    # print(mean_squared_error(y_test, y_pred))
+    # print(mean_squared_error(y_Test, y_pred))
 
     # 1 is perfect fit
-    # print(r2_score(y_test, y_pred))
+    # print(r2_score(y_Test, y_pred))
 
-    # plot_regression(x_test, y_test, y_pred, 'Regression: All Countries')
+    # plot_regression(x_test, y_Test, y_pred, 'Regression: All Countries')
 
     # REGRESSION ON UNITED STATES ##########################################
     modified_data = np.concatenate((gdp, dr, index), axis=1)
@@ -206,61 +267,72 @@ def regression(gdp, dr, index):
     # decide whether to keep or discard log scale for single country
     # mod_gdp = np.log10(mod_gdp)
 
-    x_train, x_test, y_train, y_test = train_test_split(mod_dr, mod_gdp, test_size=0.50)
+    x_train, x_test, y_Train, y_Test = train_test_split(mod_dr, mod_gdp, test_size=0.50)
     regr = linear_model.LinearRegression()
-    regr.fit(x_train, y_train)
+    regr.fit(x_train, y_Train)
     y_pred = regr.predict(x_test)
 
     # 1 is perfect fit
-    # print(r2_score(y_test, y_pred))
+    # print(r2_score(y_Test, y_pred))
 
-    # plot_regression(x_test, y_test, y_pred, 'Regression: U.S.')
+    # plot_regression(x_test, y_Test, y_pred, 'Regression: U.S.')
 
 
 def single_year_regression(gdp, dr):
     # get 2020 data
-    GDP = gdp.loc[:, gdp.columns.isin(['2020'])]
-    DR = dr.loc[:, dr.columns.isin(['2020'])]
+    GDP_ = gdp.loc[:, gdp.columns.isin(['2020'])]
+    DR_ = dr.loc[:, dr.columns.isin(['2020'])]
 
-    data = pd.merge(GDP, DR, right_index=True, left_index=True).dropna().to_numpy()
+    data = pd.merge(GDP_, DR_, right_index=True, left_index=True).dropna().to_numpy()
     data = np.delete(data, np.where(data[:, 0] == -1.0), axis=0)
     data = np.delete(data, np.where(data[:, 0] > 2E10), axis=0)
 
-    GDP, DR = np.hsplit(data, 2)
-    GDP = np.log10(GDP)
+    GDP_, DR_ = np.hsplit(data, 2)
+    GDP_ = np.log10(GDP_)
 
     # regression
-    x_train, x_test, y_train, y_test = train_test_split(DR, GDP)
+    x_train, x_test, y_Train, y_Test = train_test_split(DR_, GDP_)
     regr = linear_model.LinearRegression()
-    regr.fit(x_train, y_train)
+    regr.fit(x_train, y_Train)
     y_pred = regr.predict(x_test)
 
     # 1 is perfect fit
-    # print(r2_score(y_test, y_pred))
+    # print(r2_score(y_Test, y_pred))
 
-    # plot_regression(x_test, y_test, y_pred, 'Regression: All Countries Year 2020')
+    # plot_regression(x_test, y_Test, y_pred, 'Regression: All Countries Year 2020')
 
 
 def kmeans_clustering(gdp, dr, index):
-
     data = np.concatenate((gdp, dr, index), axis=1)
     data = pd.DataFrame(data, columns=['GDP', 'DR', 'Index']).dropna().to_numpy()
     data = np.delete(data, np.where(data[:, 0] == -1.0), axis=0)
     x1, x2, y = np.hsplit(data, 3)
     X = np.concatenate((x1, x2), axis=1)
-
-
+    kmeans = KMeans(n_clusters=len(index))
 
 
 if __name__ == '__main__':
-    df_GDP, df_DR = scrape()
+    df_GDP, df_DR, df_GDP1, df_DR1 = scrape()
     timeSeriesDR(54, df_DR)
     timeSeriesGDP(76, df_GDP)
     timeSeriesGDPregions(df_GDP)
     timeSeriesDRregions(df_DR)
 
+    # ############################################################
     total_gdp, total_dr, total_country_index = get_total(df_GDP, df_DR)
 
     regression(total_gdp, total_dr, total_country_index)
     single_year_regression(df_GDP, df_DR)
     kmeans_clustering(total_gdp, total_dr, total_country_index)
+
+    # #############################################################
+    GDP, DR = cleanData(df_GDP1, df_DR1)
+    X, y = getXy(GDP, DR)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    MLPandcm(X_train, X_test, y_train, y_test)
+    DTandcm(X_train, X_test, y_train, y_test)
+
+    Xavg = getAvg(GDP, DR)
+    X_avg_train, X_avg_test, y_avg_train, y_avg_test = train_test_split(Xavg.transpose(), y, test_size=0.2)
+    MLPandcm(X_avg_train, X_avg_test, y_avg_train, y_avg_test)
+    DTandcm(X_avg_train, X_avg_test, y_avg_train, y_avg_test)
